edc7c297245fbf4994153163a8d9215439d30d04
Stackingrule <messageswitching@gmail.com>
2019-12-10 22:29:22 +0800

    add url

---
+++ src/helpers/url.ts
@@ -0,0 +1,32 @@
+import { isDate, isObject} from 'util'
+
+function encode(val: string) : string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+
+export function buildURL(url: string, params?: any) {
+
+  if(!params) {
+    return url
+  }
+
+  const parts: string[] = []
+
+  Object.keys(params).forEach((key) => {
+    let val = params[key]
+
+
+  })
+
+
+
+
+}
